7dfdac6179c554b2e1e9a38d119bd10f9e8dba37
aparece1241 <apareceh7@gmail.com>
2020-11-23 15:16:00 +0800

    adds some changes harvey's_branch

--- models/Exam.js
+++ models/Exam.js
@@ -1,3 +1,16 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const ExamSchema = new Schema({
+    author: {type: Schema.Types.ObjectId, ref: "User"},
+    title: {type: String, required: true},
+    questions: [{type: Schema.Types.ObjectId, ref: 'Question'}],
+    status: {type: String, enum:["activates","activated","deactivated"],default: "activates"},
+    timeLimit: {type: Date, required: true},
+    expireDate: {type: Date, required: true},
+    createdAt: {type: Date, default: new Date()},
+    deletedAt: {type: Date, default: null}
+});
+
+const ExamModel = mongoose.model('Exam', ExamSchema);
+module.exports = ExamModel;
