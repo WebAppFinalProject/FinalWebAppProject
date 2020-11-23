50ddc0f1d83ad7998c4e17f79d162547903ee5ce
aparece1241 <apareceh7@gmail.com>
2020-11-23 10:06:08 +0800

    changes from master branch

--- routes/DirectRoute.js
+++ routes/DirectRoute.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const Direct = require('../controllers/DirectRoute');
 
 router.get('/',Direct.getIndex);
+router.get('/dashboard', Direct.getDashboard);
 
 module.exports = router;
