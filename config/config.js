2e67583020bfc7a42ee463c379912ba3ed880f53
Jan_Patrick Tancinco <janpatricktancinco@gmail.com>
2021-11-18 01:03:08 +0800

    [UPDATE] fix error

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
-const dbUri = "mongodb://admin:admin@cluster0-shard-00-00.wn0yq.mongodb.net:27017,"+
-"cluster0-shard-00-01.wn0yq.mongodb.net:27017,cluster0-shard-00-02.wn0yq.mongodb.net:27017/webapp_database" +
-"?ssl=true&replicaSet=atlas-qwj52s-shard-0&authSource=admin&retryWrites=true&w=majority";
+// const dbUri = "mongodb://admin:admin@cluster0-shard-00-00.wn0yq.mongodb.net:27017,"+
+// "cluster0-shard-00-01.wn0yq.mongodb.net:27017,cluster0-shard-00-02.wn0yq.mongodb.net:27017/webapp_database" +
+// "?ssl=true&replicaSet=atlas-qwj52s-shard-0&authSource=admin&retryWrites=true&w=majority";
+const dbUri = "mongodb://admin:admin@cluster0-shard-00-00.ro4is.mongodb.net:27017,cluster0-shard-00-01.ro4is.mongodb.net:27017,cluster0-shard-00-02.ro4is.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-b0d7by-shard-0&authSource=admin&retryWrites=true&w=majority"
 
 const dbUriv1 = "mongodb://localhost/webapp_database";
 
